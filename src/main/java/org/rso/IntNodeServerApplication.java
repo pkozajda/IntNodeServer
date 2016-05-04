b60b14f0bf9805c96d86c04b12bb07170b228fc0
mstelmas <marcinstel.code@gmail.com>
2016-05-04 14:33:40 +0200

    Initial commit

---
+++ src/main/java/org/rso/IntNodeServerApplication.java
@@ -0,0 +1,12 @@
+package org.rso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntNodeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntNodeServerApplication.class, args);
+	}
+}
