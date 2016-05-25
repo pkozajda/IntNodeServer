108bcaaa976f0a450d8c231a3beacf0224866d2b
Radosław <r2dek93@gmail.com>
2016-05-25 19:36:11 +0200

    population beginig

--- src/main/java/org/rso/utils/DataBasePopulator.java
+++ src/main/java/org/rso/utils/DataBasePopulator.java
@@ -2,12 +2,17 @@ package org.rso.utils;
 
 import org.rso.dto.UniversityDto;
 import org.rso.mongo.dto.GraduateDto;
-import org.rso.mongo.entities.FieldOfStudy;
+import org.rso.mongo.entities.Graduate;
+import org.rso.mongo.entities.University;
 import org.rso.mongo.repo.UniversityMongoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 /**
  * Created by Radosław on 25.05.2016.
@@ -25,31 +30,41 @@ public class DataBasePopulator {
     }
 
     private void populateDB() {
-        GraduateDto graduateDto = new GraduateDto("radek","bolek",ComeFrom.SMALL_TOWN,Location.MALOPOLSKIE,
-                new FieldOfStudy("Inf"),new UniversityDto(
-                "UJ",
-                "1364",
-                Location.MALOPOLSKIE,
-                UniversityType.UNIVERSITY
-        ));
-        GraduateDto tomek = new GraduateDto("tomek","taboret",ComeFrom.SMALL_TOWN,Location.MALOPOLSKIE,
-                new FieldOfStudy("Inf"),new UniversityDto(
-                "UJ",
-                "1364",
-                Location.MALOPOLSKIE,
-                UniversityType.UNIVERSITY
-        ));
-        GraduateDto lukasz = new GraduateDto("lukasz","szafka",ComeFrom.SMALL_TOWN,Location.MALOPOLSKIE,
-                new FieldOfStudy("Inf"),new UniversityDto(
-                "PW",
-                "1364",
-                Location.MALOPOLSKIE,
-                UniversityType.UNIVERSITY
-        ));
+        for (University university: getUniversities()){
+            Random random = new Random();
+            int val = random.nextInt(100)+1000;
+            List<GraduateDto> graduateDtoList = new ArrayList<>();
+            for (int i = 0; i < val; i++) {
+                Graduate graduate = this.createGraduate();
+                university.getGraduates().add(graduate);
+            }
+            this.universityRepository.insert(university);
+        }
+    }
 
+    private Graduate createGraduate() {
 
-        universityRepository.insertGraduate(graduateDto);
-        universityRepository.insertGraduate(lukasz);
-        universityRepository.insertGraduate(tomek);
+    }
+
+    private List<University> getUniversities(){
+        return Arrays.asList(
+                new University("PW","1820",Location.MAZOWIECKIE,UniversityType.POLYTECHNIC),
+                new University("UJ","1364",Location.MALOPOLSKIE,UniversityType.UNIVERSITY),
+                new University("SGGW","1820",Location.MAZOWIECKIE,UniversityType.AGRICULTURAL_UNIVERSITY),
+                new University("PK","1820",Location.MALOPOLSKIE,UniversityType.POLYTECHNIC),
+                new University("Politechnika Wroclawska","1920",Location.DOLNOSLASKIE,UniversityType.POLYTECHNIC),
+                new University("Politechnika Gdanska","1930",Location.POMORSKIE,UniversityType.POLYTECHNIC),
+                new University("UW","1820",Location.MAZOWIECKIE,UniversityType.UNIVERSITY),
+                new University("SGH","1876",Location.MAZOWIECKIE,UniversityType.UNIVERSITY_OF_ECONOMICS),
+                new University("Politechnika Swietokrzyska","1820",Location.SWIETOKRZYSKIE,UniversityType.POLYTECHNIC),
+                new University("UJK","1820",Location.SWIETOKRZYSKIE,UniversityType.UNIVERSITY),
+                new University("AWF Katowice","1820",Location.SLASKIE,UniversityType.OTHER),
+                new University("Uniwersytet Slaski","1820",Location.SLASKIE,UniversityType.UNIVERSITY),
+                new University("UP","1820",Location.MALOPOLSKIE,UniversityType.UNIVERSITY),
+                new University("AGH","1820",Location.MALOPOLSKIE,UniversityType.POLYTECHNIC),
+                new University("Uniwersytet Wroclawski","1820",Location.DOLNOSLASKIE,UniversityType.UNIVERSITY),
+                new University("Politechnika Rzeszowska","1820",Location.PODKARPADZKIE,UniversityType.POLYTECHNIC),
+                new University("Uniwersytet Ekonomiczny w Krakowie","1820",Location.MALOPOLSKIE,UniversityType.UNIVERSITY)
+        );
     }
 }
