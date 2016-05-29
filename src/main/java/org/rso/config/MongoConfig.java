5f543f57772b4098aa859be357f8c3db4519124a
Radosław <r2dek93@gmail.com>
2016-05-29 14:02:12 +0200

    change to fongo

--- src/main/java/org/rso/config/MongoConfig.java
+++ src/main/java/org/rso/config/MongoConfig.java
@@ -1,10 +1,10 @@
 package org.rso.config;
 
+import com.github.fakemongo.Fongo;
 import com.mongodb.Mongo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
-import org.springframework.data.mongodb.core.MongoTemplate;
 
 /**
  * Created by Radosław on 24.05.2016.
@@ -18,12 +18,12 @@ public class MongoConfig extends AbstractMongoConfiguration {
 
     @Bean
     public Mongo mongo() throws Exception {
-//        return new Fongo(getDatabaseName()).getMongo();
-        return new Mongo();
+        return new Fongo(getDatabaseName()).getMongo();
+//        return new Mongo();
     }
 
-    @Bean
-    public MongoTemplate mongoTemplate() throws Exception {
-        return new MongoTemplate(mongo(),getDatabaseName());
-    }
+//    @Bean
+//    public MongoTemplate mongoTemplate() throws Exception {
+//        return new MongoTemplate(mongo(),getDatabaseName());
+//    }
 }
